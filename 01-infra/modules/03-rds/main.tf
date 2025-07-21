f35c4159105c7ec261a4596ea0559d31439a23bc
SerhiiMyronets <hardmail88@gmail.com>
2025-07-21 14:08:27 -0700

    init

--- 01-infra/modules/03-rds/main.tf
+++ 01-infra/modules/03-rds/main.tf
@@ -63,22 +63,3 @@ resource "aws_security_group_rule" "egress_all" {
   security_group_id = aws_security_group.this.id
 }
 
-resource "aws_secretsmanager_secret" "db" {
-  name        = "${local.identifier}-credentials"
-  description = "Credentials for RDS instance ${local.identifier}"
-
-  lifecycle {
-    prevent_destroy = true
-  }
-}
-
-resource "aws_secretsmanager_secret_version" "db" {
-  secret_id = aws_secretsmanager_secret.db.id
-  secret_string = jsonencode({
-    username = var.rds_config.db_username
-    password = random_password.db_password.result
-    host     = aws_db_instance.this.address
-    port     = var.rds_config.port
-    dbname   = var.rds_config.db_name
-  })
-}
---
+++ 01-infra/modules/03-rds/secret.tf
@@ -0,0 +1,19 @@
+resource "aws_secretsmanager_secret" "db" {
+  name        = "${local.identifier}-credentials"
+  description = "Credentials for RDS instance ${local.identifier}"
+
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+
+resource "aws_secretsmanager_secret_version" "db" {
+  secret_id = aws_secretsmanager_secret.db.id
+  secret_string = jsonencode({
+    username = var.rds_config.db_username
+    password = random_password.db_password.result
+    host     = aws_db_instance.this.address
+    port     = var.rds_config.port
+    dbname   = var.rds_config.db_name
+  })
+}
