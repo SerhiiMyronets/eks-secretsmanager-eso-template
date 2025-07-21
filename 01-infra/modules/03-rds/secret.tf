a8fccd2ed3430cc8778ebd2ce13b408edb9accf7
SerhiiMyronets <hardmail88@gmail.com>
2025-07-21 14:48:24 -0700

    refactoring

--- 01-infra/modules/03-rds/secret.tf
+++ 01-infra/modules/03-rds/secret.tf
@@ -12,6 +12,7 @@ resource "aws_secretsmanager_secret_version" "db" {
   secret_string = jsonencode({
     username = var.rds_config.db_username
     password = random_password.db_password.result
+    engine   = var.rds_config.engine
     host     = aws_db_instance.this.address
     port     = var.rds_config.port
     dbname   = var.rds_config.db_name
